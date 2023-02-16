f9bd73f28454cf37ca3e22d9eb388c7bac28f340
Jyoti Potdar <JPotdar@ax.local>
2023-02-16 18:37:01 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ContactManagerComponent } from './Components/contact-manager/contact-manager.component';
+import { AddContactComponent } from './Components/add-contact/add-contact.component';
+import { EditContactComponent } from './Components/edit-contact/edit-contact.component';
+import { ViewContactComponent } from './Components/view-contact/view-contact.component';
+import { SpinnerComponent } from './Components/spinner/spinner.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ContactManagerComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ViewContactComponent,
+    SpinnerComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
